21962dc4e8b3f06fa801ccc16b97b584a88e3b67
Nazar Kuzmenko <nazar@streamcrease.com>
2013-08-17 15:28:35 +0300

    core: tabs to spaces

--- src/core.js
+++ src/core.js
@@ -1,146 +1,146 @@
 define(function ( require ) {
 
-	var Program	 	= require( 'modules/Program' ),
-		Observer	= require( 'modules/Observer' ),
-		History		= require( 'modules/History' );
+    var Program     = require( 'modules/Program' ),
+        Observer    = require( 'modules/Observer' ),
+        History     = require( 'modules/History' );
 
-	var Bash = (function () {
+    var Bash = (function () {
 
-		var _programs = {};
+        var _programs = {};
 
-		function Bash ( settings ) {
+        function Bash ( settings ) {
 
-			var self = this;
+            var self = this;
 
-			// TODO: remove this
-			Observer.on({
-				'core:init'				: function () {
-					console.log( '[OBSERVER] core:init' );
-				},
-				'core:addProgram'		: function ( data ) {
-					console.log( '[OBSERVER] core:addProgram', data.name );
-				},
-				'core:removeProgram'	: function ( data ) {
-					console.log( '[OBSERVER] core:removeProgram', data.name );
-				},
-				'core:beforeExecute'	: function ( data ) {
-					//console.log( '[OBSERVER] core:beforeExecute', data.command, data.arguments );
-				},
-				'core:afterExecute'		: function ( data ) {
-					//console.log( '[OBSERVER] core:afterExecute', data.command, data.arguments );
-				},
-				'core:echo'				: function ( data ) {
-					//console.log( '[OBSERVER] core:echo', data.data );
-					console.log( data.data );
-				},
-				'core:error'			: function ( data ) {
-					console.error( data.text );
-				}
-			});
+            // TODO: remove this
+            Observer.on({
+                'core:init'             : function () {
+                    console.log( '[OBSERVER] core:init' );
+                },
+                'core:addProgram'       : function ( data ) {
+                    console.log( '[OBSERVER] core:addProgram', data.name );
+                },
+                'core:removeProgram'    : function ( data ) {
+                    console.log( '[OBSERVER] core:removeProgram', data.name );
+                },
+                'core:beforeExecute'    : function ( data ) {
+                    //console.log( '[OBSERVER] core:beforeExecute', data.command, data.arguments );
+                },
+                'core:afterExecute'     : function ( data ) {
+                    //console.log( '[OBSERVER] core:afterExecute', data.command, data.arguments );
+                },
+                'core:echo'             : function ( data ) {
+                    //console.log( '[OBSERVER] core:echo', data.data );
+                    console.log( data.data );
+                },
+                'core:error'            : function ( data ) {
+                    console.error( data.text );
+                }
+            });
 
-			//self.history = new History();
+            //self.history = new History();
 
-			self.programs = {
+            self.programs = {
 
-				add: function ( options ) {
+                add: function ( options ) {
 
-					if ( !this.validate( 'add', options ) ) { return false; }
+                    if ( !this.validate( 'add', options ) ) { return false; }
 
-					_programs[ options.name ] = new Program( options );
+                    _programs[ options.name ] = new Program( options );
 
-					Observer.trigger( 'core:addProgram', { name: options.name } );
+                    Observer.trigger( 'core:addProgram', { name: options.name } );
 
-				},
+                },
 
-				remove: function ( name ) {
+                remove: function ( name ) {
 
-					if ( !this.validate( 'remove', options ) ) { return false; }
+                    if ( !this.validate( 'remove', options ) ) { return false; }
 
-					_programs[ name ] = undefined;
+                    _programs[ name ] = undefined;
 
-					Observer.trigger( 'core:removeProgram', { name: name } );
+                    Observer.trigger( 'core:removeProgram', { name: name } );
 
-				},
+                },
 
-				get: function () {
+                get: function () {
 
-					return _.keys( _programs );
+                    return _.keys( _programs );
 
-				},
+                },
 
-				help: function ( name ) {
+                help: function ( name ) {
 
-					if ( !this.validate( 'help', name ) ) { return false; }
+                    if ( !this.validate( 'help', name ) ) { return false; }
 
-					return _programs[ name ].help || 'The program ' + name + ' does not have help info';
+                    return _programs[ name ].help || 'The program ' + name + ' does not have help info';
 
-				},
+                },
 
-				validate: function ( method, data ) {
+                validate: function ( method, data ) {
 
-					var error = false;
+                    var error = false;
 
-					switch ( method ) {
-						case 'add':
-							error = ( data.name && data.name.length ) ? false : 'name is required';
-							break;
-						case 'remove':
-							error = ( data && _programs[ data ] ) ? false : data + ' not found';
-							break;
-						case 'help':
-							error = ( _programs[ data ] && _programs[ data ].help ) ? false : 'The program ' + data + ' does not exist or does not have help info';
-							break;
-					}
+                    switch ( method ) {
+                        case 'add':
+                            error = ( data.name && data.name.length ) ? false : 'name is required';
+                            break;
+                        case 'remove':
+                            error = ( data && _programs[ data ] ) ? false : data + ' not found';
+                            break;
+                        case 'help':
+                            error = ( _programs[ data ] && _programs[ data ].help ) ? false : 'The program ' + data + ' does not exist or does not have help info';
+                            break;
+                    }
 
-					if ( error ) {
-						Observer.trigger( 'core:error', { text: 'Programs ' + method + ': ' + error } );
-						return false;
-					} else {
-						return true;
-					}
+                    if ( error ) {
+                        Observer.trigger( 'core:error', { text: 'Programs ' + method + ': ' + error } );
+                        return false;
+                    } else {
+                        return true;
+                    }
 
-				}
+                }
 
-			};
+            };
 
-			Observer.trigger( 'core:init' );
+            Observer.trigger( 'core:init' );
 
-		}
+        }
 
-		Bash.prototype.exec = function ( commandString ) {
-			
-			var args		= commandString.split( ' ' ),
-				command		= args.shift(),
-				output;
+        Bash.prototype.exec = function ( commandString ) {
+            
+            var args        = commandString.split( ' ' ),
+                command     = args.shift(),
+                output;
 
-			//this.history.add( commandString );
+            //this.history.add( commandString );
 
-			Observer.trigger( 'core:beforeExecute', { command: command, arguments: args } );
+            Observer.trigger( 'core:beforeExecute', { command: command, arguments: args } );
 
-			if ( _programs[ command ] ) {
-				output = _programs[ command ].run( args );
-			} else {
-				Observer.trigger( 'core:error', { text: command + ': command not found' } );
-			}
+            if ( _programs[ command ] ) {
+                output = _programs[ command ].run( args );
+            } else {
+                Observer.trigger( 'core:error', { text: command + ': command not found' } );
+            }
 
-			Observer.trigger( 'core:afterExecute', { command: command, arguments: args, output: output } );
+            Observer.trigger( 'core:afterExecute', { command: command, arguments: args, output: output } );
 
-			return output;
+            return output;
 
-		};
+        };
 
-		Bash.prototype.echo = function ( str ) {
+        Bash.prototype.echo = function ( str ) {
 
-			Observer.trigger( 'core:echo', { data: str } );
+            Observer.trigger( 'core:echo', { data: str } );
 
-			return this;
+            return this;
 
-		};
+        };
 
-		return Bash;
+        return Bash;
 
-	})();
+    })();
 
-	return Bash;
+    return Bash;
 
 });
